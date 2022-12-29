204eb1a95c3be4ce86be8bc59e6dc9f2738f2f2b
Maksym Tsapiv <maksym.tsapiv@ucu.edu.ua>
2022-12-29 21:06:09 +0200

    merge

--- monitor_firewall/listener.py
+++ monitor_firewall/listener.py
@@ -1,25 +1,32 @@
-
 import netifaces
 import subprocess
 import os
 import sys
+import warnings
+warnings.filterwarnings('ignore')
 
 
 project_dir = os.path.realpath(os.path.dirname(__file__))
 os.chdir(os.path.join(project_dir, "monitorBX"))
 
 
-interfaces = [i for i in range(2, len(netifaces.interfaces()[1:]) + 2)]
-str_interfaces = ""
-for elm in interfaces:
-    str_interfaces += f"{elm} "
+interfaces = [str(i) for i in range(2, len(netifaces.interfaces()[1:]) + 2)]
 
 not_bannable_ip = ["0.0.0.0", "127.0.0.1", "127.0.0", "192.168", "10.0.1.1", "10.10.1.1"]
 
-cmd = [f"{os.path.join(project_dir, 'monitorBX', 'monitorBX')} {' '.join(str(i) for i in interfaces)}"]
+cmd = [f"sudo {os.path.join(project_dir, 'monitorBX', 'monitorBX')} {' '.join(interfaces)}"]
 
 
 ips_to_ban = []
+
+
+def block(interfaces, address):
+    os.chdir(os.path.join(project_dir, "Heimdallr-project"))
+    for interface in interfaces:
+        subprocess.call(["sudo python3", "hmdl.py", "-s", interface])
+        subprocess.call(["sudo", "python3", "hmdl.py", "-A"])
+        subprocess.call(["sudo", "python3", "hmdl.py", "-d", address])
+    os.chdir(os.path.join(project_dir, "monitorBX"))
 
 def execute(cmd):
     popen = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -37,17 +44,15 @@ def execute(cmd):
                 while True:
                     to_ban = input(f"Do you want to ban ip {ip}? y\\n: ")
                     if to_ban == "y":
-                        to_ban = True
+                        block(interfaces, ip)
                         break
                     elif to_ban == "n":
-                        to_ban = False
                         break
                     else:
                         print("Wrong input, please try again")
 
-
-            if flag_to_ban and to_ban:
-                ips_to_ban.append(ip)
+            # if flag_to_ban and to_ban:
+            #     ips_to_ban.append(ip)
     popen.stdout.close()
     return_code = popen.wait()
     if return_code:
@@ -56,5 +61,3 @@ def execute(cmd):
 
 
 execute(cmd)
-
-
